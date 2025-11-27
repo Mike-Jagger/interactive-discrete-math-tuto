f2a625fe07a5700c54e4fb85b2755ff7a8cc53f7
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2025-11-27 13:18:47 -0700

    adding initial project files

---
+++ src/main/java/org/dstgroup8/interactivediscretemathtuto/InteractiveDiscreteMathTutoApplication.java
@@ -0,0 +1,13 @@
+package org.dstgroup8.interactivediscretemathtuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveDiscreteMathTutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InteractiveDiscreteMathTutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dstgroup8/interactivediscretemathtuto/InteractiveDiscreteMathTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dstgroup8.interactivediscretemathtuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractiveDiscreteMathTutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
